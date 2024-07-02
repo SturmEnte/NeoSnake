bdf792bd7f8d85a5645e72bb4606364b510b72c6
Jonas <jonas@sturmente.de>
2024-07-02 21:34:08 +0200

    Changed field size for testing

--- NeoSnake/NeoSnake.cs
+++ NeoSnake/NeoSnake.cs
@@ -2,13 +2,13 @@ namespace NeoSnake
 {
     public partial class SnakeForm : Form
     {
-        const int FIELD_COUNT_X = 8;
-        const int FIELD_COUNT_Y = 8;
+        const int FIELD_COUNT_X = 4;
+        const int FIELD_COUNT_Y = 4;
 
         const int FIELD_WIDTH = 50;
         const int FIELD_HEIGHT = 50;
 
-        const int START_BODY_ELEMENTS = 3; // TBD: If less than 1, it will crash
+        const int START_BODY_ELEMENTS = 1; // TBD: If less than 1, it will crash
 
         public SnakeForm()
         {
@@ -17,7 +17,7 @@ namespace NeoSnake
 
         private void SnakeForm_Load(object sender, EventArgs e)
         {
-            Game game = new Game(FIELD_COUNT_X, FIELD_COUNT_Y, FIELD_WIDTH, FIELD_HEIGHT, 500, 2, this);
+            Game game = new Game(FIELD_COUNT_X, FIELD_COUNT_Y, FIELD_WIDTH, FIELD_HEIGHT, 500, START_BODY_ELEMENTS, this);
         }
     }
 }
