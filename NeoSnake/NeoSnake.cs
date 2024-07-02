0ad7a66fda3a7ca192b5c36fedf198e7fa21d8bc
Jonas <jonas@sturmente.de>
2024-07-02 21:00:40 +0200

    Moved position class to own file

--- NeoSnake/NeoSnake.cs
+++ NeoSnake/NeoSnake.cs
@@ -281,13 +281,4 @@ namespace NeoSnake
             }
         }
     }
-
-    class Position
-    {
-        public int x, y;
-        public Position(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
 }
---
+++ NeoSnake/Position.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeoSnake
+{
+    internal class Position
+    {
+        public int x, y;
+        public Position(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
