726fc481b8bdfe44c3390e650b3b4aea78d19c9f
Jonas <jonas@sturmente.de>
2024-07-03 11:55:30 +0200

    Created Looks class

---
+++ NeoSnake/Looks.cs
@@ -0,0 +1,31 @@
+﻿namespace NeoSnake
+{
+    internal class Looks
+    {
+        private PictureBox head;
+        private PictureBox body;
+        private PictureBox apple;
+
+        Looks(PictureBox head, PictureBox body, PictureBox apple)
+        {
+            this.head = head;
+            this.body = body;
+            this.apple = apple;
+        }
+
+        public PictureBox GetHead()
+        {
+            return head;
+        }
+
+        public PictureBox GetBody()
+        {
+            return body;
+        }
+
+        public PictureBox GetApple()
+        {
+            return apple;
+        }
+    }
+}
