ef81b1d981a5ca67016b1797f82d111344b28d85
Jonas <jonas@sturmente.de>
2024-07-03 18:50:41 +0200

    Added basic cc look implementation

--- NeoSnake/LookManager.cs
+++ NeoSnake/LookManager.cs
@@ -28,8 +28,29 @@
             defaultApple.BorderStyle = BorderStyle.FixedSingle;
             defaultApple.BackColor = Color.DarkRed;
 
-            Looks defaultLooks = new Looks(defaultGameTile, defaultHead, defaultBody, defaultApple);
-            looks.Add("Default", defaultLooks);
+            Looks defaultLook = new Looks(defaultGameTile, defaultHead, defaultBody, defaultApple);
+            looks.Add("Clear_code Assets", defaultLook);
+
+            // Clean_code Assets
+            PictureBox ccTile = new PictureBox();
+            ccTile.BorderStyle = BorderStyle.None;
+            ccTile.BackColor = Color.Green;
+
+            PictureBox ccHead = new PictureBox();
+            defaultHead.BorderStyle = BorderStyle.FixedSingle;
+            defaultHead.BackColor = Color.DarkGreen;
+
+            PictureBox ccBody = new PictureBox();
+            defaultBody.BorderStyle = BorderStyle.FixedSingle;
+            defaultBody.BackColor = Color.Green;
+
+            PictureBox ccApple = new PictureBox();
+            ccApple.BorderStyle = BorderStyle.None;
+            ccApple.Image = Image.FromFile("apple.png");
+            ccApple.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            Looks ccLook = new Looks(ccTile, ccHead, ccBody, ccApple);
+            looks.Add("Clear_code Assets", ccLook);
         }
 
         public string GetDefaultLooksName()
